fe2ae0018b4da40945566e908194c7c0ffa1dbfd
Yazdun <yazduntube@gmail.com>
2023-08-07 11:27:35 +0330

    add colors to main

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import "./style.css";
 
+// ⚠️ YOU REMOVE THIS LATER ⚠️
+const colors = [
+  "#007bff",
+  "#f1c40f",
+  "#27ae60",
+  "#e74c3c",
+  "#8e44ad",
+  "#3498db",
+  "#f39c12",
+];
+
 document.querySelector("#app").innerHTML = `
   <div>
     <button id="flipper" type="button">Start Flipping</button>
